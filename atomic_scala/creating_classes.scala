e7ec242b08603a189acdcc9c83a75d25c6ec993c
kang <2881570+kangni@users.noreply.github.com>
2019-12-08 22:55:49 +0800

    add classes

---
+++ atomic_scala/creating_classes.scala
@@ -0,0 +1,27 @@
+class Giraffe
+class Bear
+class Hippo
+
+val g1 = new Giraffe
+val g2 = new Giraffe
+val b = new Bear
+val h = new Hippo
+
+println(g1)
+println(g2)
+println(b)
+println(h)
+
+//
+class Hyena {
+    println("This is in the class body.")
+}
+
+val hyena = new Hyena
+
+//
+class Zebra {
+    println("I have stripes.")
+}
+
+val z = new Zebra
