aea7d709f80dc8bc3901e7ee586518e6e214c01c
Kang <2881570+kangni@users.noreply.github.com>
2019-12-15 23:26:29 +0800

    Test package

---
+++ atomic_scala/AtomicTest.scala
@@ -0,0 +1,40 @@
+package com.atomicscala
+import language.implicitConversions
+import java.io.FileWriter
+
+class AtomicTest[T](val target:T) {
+    val errorLog = "_AtomicTestErrors.log"
+    def tst[E](expected:E)(test: => Boolean) {
+        println(target)
+        if (test == false) {
+            val msg = "[Error] expected:\n" +
+            expected
+            println(msg)
+            val el = new FileWriter(errorLog, true)
+            el.write(target + msg + "\n")
+            el.close()
+        }
+    }
+
+    def str = // Safely convert to a String
+      Option(target).getOrElse("").toString
+
+    def is(expected:String) = tst(expected) {
+        expected.replaceAll("\r\n", "") == str
+    }
+
+    def is[E](expected:E) = tst(expected) {
+        expected == target
+    }
+
+    def beginsWith(exp:String) = tst(exp) {
+        str.startsWith(
+            exp.replaceAll("\r\n", "\n")
+        )
+    }
+}
+
+object AtomicTest {
+    implicit def any2Atomic[T](target:T) = 
+      new AtomicTest(target)
+}
